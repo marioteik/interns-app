4332a7f5d8a1ae84956715776fe24886137c9bf1
sheyla pariona <sheylapariona@gmail.com>
2018-02-24 19:13:35 -0300

    select-type component - html e css

---
+++ client/app/select-type-component/select-type.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-select-type',
+  templateUrl: './select-type.component.html',
+  styleUrls: ['./select-type.component.css']
+})
+export class SelectTypeComponent implements OnInit {
+
+}
