ce3fda149134b9e0b4ced45c6ab80f738eef34c2
Wallison777 <36735259+Wallison777@users.noreply.github.com>
2018-02-25 21:51:21 -0300

    datasubmit criado

---
+++ client/app/components/datasubmit/datasubmit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-datasubmit',
+  templateUrl: './datasubmit.component.html',
+  styleUrls: ['./datasubmit.component.css']
+})
+export class DatasubmitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
